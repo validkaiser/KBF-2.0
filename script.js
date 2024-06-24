f134bd928903bdfbb1877a2e4e258a48561134b5
validkaiser <94759357+validkaiser@users.noreply.github.com>
2024-06-24 22:48:47 +0300

    update

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function openMenu() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
+
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
